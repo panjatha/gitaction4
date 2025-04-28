dbcfd8af9468483f576273e30cf3349081637525
panjatha <panjatha.mandeep@gmail.com>
2025-04-28 11:42:48 +0530

    new one

---
+++ mandeep.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.27.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "data_rg"
+    storage_account_name = "sksgit"
+    container_name       = "mandeeps"
+    key                  = "mandeep.tfstate"
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+resource "azurerm_resource_group" "mandeeps" {
+  name     = "mandoas"
+  location = "East US"
+
+}
+
+resource "azurerm_storage_account" "mandeepstorage" {
+  name                     = "storagemandeep"
+  resource_group_name      = azurerm_resource_group.mandeeps.name
+  location                 = azurerm_resource_group.mandeeps.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
